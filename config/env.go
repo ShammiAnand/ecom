760da3c00ead61a57b24a3b8bbb2cdbc99403270
Shammi Anand <shammianand101@gmail.com>
2024-04-18 18:53:32 +0530

    add TokenExpiry to env and made getEnv generic(kinda)

--- config/env.go
+++ config/env.go
@@ -2,18 +2,21 @@ package config
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
 	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	PublicHost string
-	Port       string
-	DBUser     string
-	DBPassword string
-	DBAddress  string
-	DBName     string
-	Secret     string
+	PublicHost  string
+	Port        string
+	DBUser      string
+	DBPassword  string
+	DBAddress   string
+	DBName      string
+	Secret      string
+	TokenExpiry int
 }
 
 var Envs = initConfig()
@@ -22,24 +25,40 @@ func initConfig() Config {
 
 	godotenv.Load()
 
+	tokenExpiry := getEnv("TOKEN_EXPIRY", 3600).(int)
+
 	return Config{
-		PublicHost: getEnv("PUBLIC_HOST", "http://localhost"),
-		Port:       getEnv("PORT", "8000"),
-		DBUser:     getEnv("DB_USER", "root"),
-		DBPassword: getEnv("DB_PASSWORD", "mypassword"),
-		DBName:     getEnv("DB_NAME", "ecom"),
+		PublicHost: getEnv("PUBLIC_HOST", "http://localhost").(string),
+		Port:       getEnv("PORT", "8000").(string),
+		DBUser:     getEnv("DB_USER", "root").(string),
+		DBPassword: getEnv("DB_PASSWORD", "mypassword").(string),
+		DBName:     getEnv("DB_NAME", "ecom").(string),
 		DBAddress: fmt.Sprintf(
 			"%s:%s",
 			getEnv("DB_HOST", "127.0.0.1"),
 			getEnv("DB_PORT", "3306"),
 		),
-		Secret: getEnv("SECRET", "some_secret"),
+		Secret:      getEnv("SECRET", "some_secret").(string),
+		TokenExpiry: tokenExpiry,
 	}
 }
 
-func getEnv(key, fallback string) string {
+func getEnv(key string, fallback interface{}) interface{} {
 	if value, ok := os.LookupEnv(key); ok {
-		return value
+		switch fallback.(type) {
+		case string:
+			return value
+		case int:
+			if intValue, err := strconv.Atoi(value); err == nil {
+				return intValue
+			}
+		case float64:
+			if floatValue, err := strconv.ParseFloat(value, 64); err == nil {
+				return floatValue
+			}
+		default:
+			return value
+		}
 	}
 	return fallback
 }
--- service/auth/jwt.go
+++ service/auth/jwt.go
@@ -16,7 +16,7 @@ import (
 
 func CreateJWT(secret []byte, userID int) (string, error) {
 
-	expiration := time.Second * time.Duration(30)
+	expiration := time.Second * time.Duration(config.Envs.TokenExpiry)
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"userID":    strconv.Itoa(userID),
 		"expiredAt": time.Now().Add(expiration).Unix(),
